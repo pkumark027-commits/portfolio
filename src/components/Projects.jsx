943cc0dea1dd78ca8091785ac5b7741aa46c1ff3
Pavan Kumar K <pkumark027@gmail.com>
2026-09-20 00:48:37 +0530

    Update project state

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -75,7 +75,7 @@ const Projects = () => {
                 "Translating complex machine learning outputs into easily understandable UI components for end-users.",
                 "Deploying heavy PyTorch/Transformer models in a lightweight Flask environment."
             ],
-            githubUrl: "https://github.com/Akhilesh6803-AM/Cyberbullying-Detection",
+            githubUrl: "https://github.com/pkumark027-commits/cyber-bulling",
             demoUrl: "https://cyberbullying-detection-ttjv.onrender.com/"
         },
         {
@@ -125,7 +125,7 @@ const Projects = () => {
                 "Managing complex responsive layouts where bold, oversized typography could easily break the container boundaries.",
                 "Structuring the application to allow for easy future integration with a real backend payment processor."
             ],
-            githubUrl: "https://github.com/Akhilesh6803-AM/E-Commerce",
+            githubUrl: "https://github.com/pkumark027-commits/e-commerce",
             demoUrl: "https://v0-ak-e-commerce.vercel.app/"
         }
     ];
